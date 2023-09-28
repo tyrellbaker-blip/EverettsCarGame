b45be6fea08b8585d1ac4bd12f0560b7afc0a51d
Tyrell Baker <tybaker1@csumb.edu>
2023-09-28 12:51:13 -0700

    Initial commit

---
+++ main_game.py
@@ -0,0 +1,40 @@
+import pygame
+import random
+import sys
+
+
+class Car:
+    def __init__(self, x, y, image):
+        pass
+
+    def draw(self, display):
+        pass
+
+    def move(self, x_change, y_change):
+        pass
+
+
+class Obstacle:
+    def __init__(self, x, y, speed, image):
+        pass
+
+    def draw(self, display):
+        pass
+
+    def move(self):
+        pass
+
+
+class Game:
+    def __init__(self, display_width, display_height):
+        pass
+
+    def spawn_obstacle(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
